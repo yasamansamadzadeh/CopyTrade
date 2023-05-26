c31f0303bcbb58ad4bddcac719d181880237368c
yasamansamadzadeh <y.samadzadeh@yahoo.com>
2023-05-26 12:25:05 +0330

    --amend

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.home'  # Enable the inner home (home)
+    'apps.home',
+    'apps.trade',
 ]
 
 MIDDLEWARE = [
