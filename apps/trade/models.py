6e06d2e916a19240589c30f30d3f1471bc291800
yasamansamadzadeh <y.samadzadeh@yahoo.com>
2023-05-26 21:29:12 +0330

    Create Account and Order models

--- apps/trade/models.py
+++ apps/trade/models.py
@@ -6,9 +6,10 @@ from apps.myauth.models import User
 class Trader(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='trader')
     is_master = models.BooleanField(default=False)
-    kc_key = models.CharField(max_length=24)
+    kc_key = models.CharField(max_length=24, unique=True)
     kc_secret = models.CharField(max_length=36)
     kc_passphrase = models.CharField(max_length=32)
+    kc_last_sync = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         verbose_name = _('trader')
@@ -32,3 +33,37 @@ class Follow(models.Model):
 
     def __str__(self):
         return "%s → %s" % (self.slave, self.master)
+
+
+class Account(models.Model):
+    trader = models.ForeignKey(Trader, on_delete=models.CASCADE, related_name='accounts')
+    kc_id = models.CharField(max_length=24, unique=True)
+    currency = models.CharField(max_length=63)
+    type = models.CharField(max_length=63)
+    balance = models.FloatField(default=.0)
+    available = models.FloatField(default=.0)
+    holds = models.FloatField(default=.0)
+
+    class Meta:
+        verbose_name = _('account')
+        verbose_name_plural = _('accounts')
+
+    def __str__(self):
+        return "%s %s" % (self.trader, self.currency)
+
+
+class Order(models.Model):
+    trader = models.ForeignKey(Trader, on_delete=models.CASCADE, related_name='orders')
+    kc_id = models.CharField(max_length=24, unique=True)
+    kc_created_at = models.DateTimeField()
+    src_currency = models.CharField(max_length=63)
+    dst_currency = models.CharField(max_length=63)
+    price = models.FloatField()
+    size = models.FloatField()
+
+    class Meta:
+        verbose_name = _('order')
+        verbose_name_plural = _('orders')
+
+    def __str__(self):
+        return "%s %s-%s" % (self.trader, self.src_currency, self.dst_currency)
