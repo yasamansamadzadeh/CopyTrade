fd465bd736bf965aa6ad574fd40050405f6b6064
yasamansamadzadeh <y.samadzadeh@yahoo.com>
2023-06-02 12:23:40 +0330

    Add unfollow functionality

--- apps/trade/views.py
+++ apps/trade/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Exists, OuterRef
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
@@ -11,15 +12,20 @@ from apps.trade.models import Trader, Follow
 def masters(request):
     context = {
         'segment': 'masters',
-        'masters': Trader.objects.filter(is_master=True),
+        'masters': Trader.objects.filter(is_master=True).annotate(
+            is_followed=Exists(Follow.objects.filter(master=OuterRef('pk'), slave=request.user.trader))),
         'is_master': hasattr(request.user, 'trader') and request.user.trader.is_master,
     }
 
     if request.method == 'POST':
+        action = request.POST.dict()['action']
         master_id = request.POST.dict()['master_id']
-        master = Trader.objects.get(id=master_id)
-        if not Follow.objects.filter(master=master, slave=request.user.trader).exists():
-            Follow.objects.create(master=master, slave=request.user.trader)
+        master = Trader.objects.get(id=master_id, is_master=True)
+        if action == 'follow':
+            if not Follow.objects.filter(master=master, slave=request.user.trader).exists():
+                Follow.objects.create(master=master, slave=request.user.trader)
+        elif action == 'unfollow':
+            Follow.objects.filter(master=master, slave=request.user.trader).delete()
 
     html_template = loader.get_template('trade/masters.html')
     return HttpResponse(html_template.render(context, request))
